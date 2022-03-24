d9e451c39c85fc94868a5358a3a94a27cf510d85
rfps09 <rafael09felipe@gmail.com>
2022-03-23 21:30:29 -0300

    InsertionSort

--- AlgoritmosDeOrdenacao/InsertionSort.cpp
+++ AlgoritmosDeOrdenacao/InsertionSort.cpp
@@ -42,7 +42,7 @@ int maior(int &a, int &b) {
 int main() {
     int vetorTeste[MAXN] = {9,7,3,8,6,2,5,1,4,0};
 
-    insertionSort(vetorTeste, vetorTeste+MAXN,maior);
+    insertionSort(vetorTeste, vetorTeste+MAXN);
 
     for(int i=0; i<MAXN; i++)
         cout << vetorTeste[i] << " ";
