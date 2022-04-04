3ab86f131b9513ff0f117ff63cafb2c6fa72d29f
RAFAEL FELIPE <rafael09felipe@gmail.com>
2022-04-04 19:25:06 -0300

    ainda não testado

--- EstruturasDeDados/VetorDinamico.cpp
+++ EstruturasDeDados/VetorDinamico.cpp
@@ -19,38 +19,52 @@ public:
 
     ~vetor() {
         delete [] vetorDinamico;
+        cout << "isso é tudo pessoal" << endl;
     }
 
     int size() {return index;}
 
     int capacity() {return length;}
 
-    void resize(int length) {
+    void reserve(int length) {
+        if(index > length) index = length;
         this->length = length;
-        vetorDinamico = (tipo*)realloc(vetorDinamico,length*(sizeof(tipo)));
-        memset(vetorDinamico+index,0,(length-index)*sizeof(tipo));
+        tipo *aux = new tipo[length]{0};
+        for(int i=0; i<index; i++)
+            aux[i]=vetorDinamico[i];
+        delete [] vetorDinamico;
+        vetorDinamico = aux;
+        if(vetorDinamico == NULL) throw invalid_argument("Deu beyblade");
+    }
+
+    void resize(int length) {
+        if(length > 0 && length < index) index = length;
+        if(length > this->length) reserve(length);
     }
 
     void push(tipo element) {
-        if(index >= length) resize(2*length);
+        if(index >= length) reserve(2*length);
         vetorDinamico[index] = element;
         index++;
     }
 
     void pop() {
+        if(index == 0) throw invalid_argument("Ta errado isso ai doidao");
         if(index>0)index--;
-
     }
 
     tipo front() {
+        if(index == 0) throw invalid_argument("num vai da nao doidao");
         return vetorDinamico[0];
     }
 
     tipo back() {
+        if(index == 0) throw invalid_argument("num vai da nao doidao");
         return vetorDinamico[index-1];
     }
 
     tipo at(int i) {
+        if(i < 0 || i >= index) throw invalid_argument("Ta perdidinho no pagode");
         return vetorDinamico[i];
     }
 
@@ -67,6 +81,7 @@ public:
     }
 
     tipo operator [] (int i) {
+        if(i < 0 || i >= index) throw invalid_argument("Ta perdidinho no pagode");
         return vetorDinamico[i];
     }
 };
@@ -80,15 +95,25 @@ int main() {
     meuVetor.push(4);
     meuVetor.push(5);
 
-    for(int i=0; i < meuVetor.capacity(); i++) {
+    for(int i=0; i < meuVetor.size(); i++) {
         cout << meuVetor[i] << " ";
     }
 
     cout << endl;
-    meuVetor.resize(2);
+    meuVetor.reserve(2);
 
-    for(int i=0; i < meuVetor.capacity(); i++) {
-        cout << meuVetor.at(i) << " ";
+    for(int i=0; i < meuVetor.size(); i++) {
+        cout << meuVetor[i] << " ";
+    }
+
+    cout << endl;
+    meuVetor.push(10);
+    meuVetor.push(11);
+    meuVetor.push(12);
+
+
+    for(int i=0; i <= meuVetor.size(); i++) {
+        cout << meuVetor[i] << " ";
     }
 
     return 0;
